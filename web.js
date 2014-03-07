9d6fc38e8b4b5027b89e7f5be0bbedd0d2f67018
Roy Rim <rrim@silversky.com>
2014-03-06 21:49:20 -0500

    added xml-stream, modified to return L status

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var http = require("http");
+var xmlStream = require('xml-stream');
+var mta = { host: 'web.mta.info', path: '/status/serviceStatus.txt' };
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  var status = 'NOT FOUND';
+  var request = http.get( mta ).on( 'response', function( response ) {
+    var xml = new xmlStream( response );
+    xml.on( 'endElement: line', function( line ) {
+      if ( line.name == 'L' ) {
+        console.log( line );
+        status = line.status;
+        res.json( { 'status': status } );
+      }
+    } );
+  } );
 });
 
 var port = Number(process.env.PORT || 5000);
