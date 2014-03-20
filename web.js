b2bfe65d7dc32e5bd2b1c708fcd784de04631c9b
Roy Rim <rrim@silversky.com>
2014-03-20 09:01:26 -0400

    including id

--- web.js
+++ web.js
@@ -3,17 +3,23 @@ var express = require("express");
 var logfmt = require("logfmt");
 var http = require("http");
 var xmlStream = require('xml-stream');
+var jade = require('jade');
 var mta = { host: 'web.mta.info', path: '/status/serviceStatus.txt' };
 var app = express();
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
+  res.render( 'index' );
+} );
+
+app.get('/status/:id', function(req, res) {
   var status = 'NOT FOUND';
   var request = http.get( mta ).on( 'response', function( response ) {
     var xml = new xmlStream( response );
     xml.on( 'endElement: line', function( line ) {
-      if ( line.name == 'L' ) {
+      if ( line.name == req.params.id ) {
         console.log( line );
         status = line.status;
         res.json( { 'status': status } );
